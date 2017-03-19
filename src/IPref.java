9691e6a9b875ef83a0ae220f4226321774fd6be1
Eugene Wang <eugene.r.w.12@gmail.com>
2017-03-18 22:45:28 -0400

    geoCorrds

--- src/IPref.java
+++ src/IPref.java
@@ -5,12 +5,22 @@ public class IPref {
 	String country;
 	String province;
 	String city;
-	
+	Geo geo;	
 	String dataCenter;
 	
+	static class Geo{
+		double lattitude = 0;
+		double longitude = 0; 
+		
+		private Geo (double x, double y){
+			lattitude = x;
+			longitude = y;
+		}		
+	}
 	
 	public IPref (){
-		
 	}
+	
+	
 	
 }
